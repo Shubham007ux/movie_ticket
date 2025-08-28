a4f5c21b7d4c7aa534599575736053f9889bc822
Shubham007ux <shubhamsrma005@gmail.com>
2025-08-28 10:37:04 +0530

    folder structure created

---
+++ src/components/shubham.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Shubham() {
+    return(
+        <div>Shubham</div>
+    )
+}
+export default Shubham;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import Shubham from '../components/shubham';
+
+const Test =()=>{
+    return(
+        <div>
+            <h1>info</h1>
+            <Shubham></Shubham>
+
+        </div>
+    )
+}
+
+export default Test;
